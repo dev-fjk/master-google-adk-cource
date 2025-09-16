520a6a83d692728e5dca083f8865053ccf6ad780
dev-fjk <fjk.develop@gmail.com>
2025-09-16 21:53:26 +0900

    feat: section2 create web page

--- agents/root_website_builder/agent.py
+++ agents/root_website_builder/agent.py
@@ -3,14 +3,14 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).resolve().parents[2]))
 
-from google.adk.agents import LlmAgent
+from google.adk.agents import SequentialAgent
 
 from agents.code_writer.agent import code_writer_agent
 from agents.designer.agent import designer_agent
 from agents.requirements_writer.agent import requirements_writer_agent
 from utils.file_loader import load_instructions_file
 
-root_agent = LlmAgent(
+root_agent = SequentialAgent(
   name = "root_website_builder_agent",
   description=load_instructions_file("agents/root_website_builder/description.txt"),
   sub_agents=[
